ba4d7df46c726a50480e5be94017ae60f0587c35
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-09-27 16:50:12 +0100

    Return a new promise object

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default function getResponseFromAPI() {
+        return new Promise((resolve, reject) => {
+                const success = true;
+                if (success) {
+                        resolve('Success');
+                } else {
+                        reject('Rejected');
+                }
+        });
+}
