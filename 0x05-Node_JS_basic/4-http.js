c26747a620110d55adb73c498b20160ee7bd484d
Mustapha Ajiroba <ajiroba980@gmail.com>
2024-10-08 06:36:30 +0000

    correction;

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,9 @@
-const { createServer } = require('node:http');
+const http = require('node:http');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = createServer((req, res) => {
+const app = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
 	res.end('Hello Holberton School!');
