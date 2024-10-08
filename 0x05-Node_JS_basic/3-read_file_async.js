c0bcdbf461f359d5cd6cb44442fe7ae36794e621
Mustapha Ajiroba <ajiroba980@gmail.com>
2024-10-08 08:29:28 +0000

    Read file asynchronously"

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,4 +1,33 @@
+const fs = require('node:fs/promises');
+
 async function countStudent(filePath) {
 	try {	
-		const data = await fs.readFile(filePath, ) {
-			
+		const data = await fs.readFile(filePath, { encoding: 'utf8'});
+		let total = 0;
+		let CS = 0;
+		let listCS = [];
+		let SWE = 0;
+		let listSWE = [];
+		const result = data.split('\n');
+		for(const iter of result) {
+			if (iter.includes('CS')) {
+				CS += 1;
+				let temp = iter.split(',');
+				listCS.push(' ' + temp[0]);
+			} else if (iter.includes('SWE')) {
+				SWE += 1;
+				let pmet = iter.split(',');
+				listSWE.push(' ' + pmet[0]);
+			} else {
+				continue;
+			}
+			total += 1
+		}
+		console.log(`Number of students: ${total}`);
+		console.log(`Number of students in CS: ${CS} List: ${listCS}`);
+	        console.log(`Number of students in SWE: ${SWE} List: ${listSWE}`);
+	} catch (err) {
+		throw new Error('Cannot load database');
+	}
+}
+module.exports = countStudent;
