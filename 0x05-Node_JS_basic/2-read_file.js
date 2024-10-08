60977f32c6823100267cee89b554c054ab4bbdcf
Mustapha Ajiroba <ajiroba980@gmail.com>
2024-10-08 09:14:55 +0000

    eslint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,4 +1,4 @@
-const fs = require('node:fs');
+const { fs } = require('node:fs');
 function countStudent(filePath) {
 	const data = fs.readFileSync(filePath, 'utf-8')
 	let total = 0;
@@ -6,24 +6,28 @@ function countStudent(filePath) {
 	let listCS = [];
 	let SWE = 0;
 	let listSWE = [];
-	const result = data.split('\n');
-	for(const iter of result) {
-		if (iter.includes('CS')) {
-			CS += 1;
-			let temp = iter.split(',');
-			listCS.push(' ' + temp[0]);
-		} else if (iter.includes('SWE')) {
-			SWE += 1;
-			let pmet = iter.split(',');
-			listSWE.push(' ' + pmet[0]);
-		} else {
-			continue;
-		}
-		total += 1
-		}
-	console.log(`Number of students: ${total}`);
-	console.log(`Number of students in CS: ${CS} List: ${listCS}`);
-	console.log(`Number of students in SWE: ${SWE} List: ${listSWE}`);
+	try {
+		const result = data.split('\n');
+		for(const iter of result) {
+			if (iter.includes('CS')) {
+				CS += 1;
+				let temp = iter.split(',');
+				listCS.push(' ' + temp[0]);
+			} else if (iter.includes('SWE')) {
+				SWE += 1;
+				let pmet = iter.split(',');	
+				listSWE.push(' ' + pmet[0]);
+			} else {
+				continue;
+			}
+			total += 1
+			}
+		console.log(`Number of students: ${total}`);
+		console.log(`Number of students in CS: ${CS} List: ${listCS}`);
+		console.log(`Number of students in SWE: ${SWE} List: ${listSWE}`);
+	} catch (err) {
+		throw new Error('Cannot load the database');
+	}
 }
 
 module.exports = countStudent;
